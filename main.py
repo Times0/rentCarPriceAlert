4dc2976363f13653a417aed205daa74c53107ca8
Times0 <45049767+Times0@users.noreply.github.com>
2024-03-10 23:40:22 +0100

    add better detection of loading

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-import time
 import pyshorteners
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
-
 from telegram_notifier import send_message
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-TRIGGER_PRICE = 700
+TRIGGER_PRICE = 520
 
 
 def main():
     # initialize an instance of the chrome driver (browser)
-    driver = uc.Chrome(browser_executable_path="./chromedriver/chromedriver_linux")
 
     # visit your target site
     link = "https://www.rentalcars.com/search-results?coordinates=44.825893%2C-0.556612&doDay=5&doHour=16&doMinute=0&doMonth=7&doYear=2024&driversAge=30&dropCoordinates=44.825893%2C-0.556612&dropFtsType=&dropLocation=-1&dropLocationIata=&dropLocationName=Gare%20de%20Bordeaux-Saint-Jean&ftsType=A&location=-1&locationIata=&locationName=Gare%20de%20Bordeaux-Saint-Jean&puDay=22&puHour=16&puMinute=0&puMonth=6&puYear=2024&filterCriteria_transmission=MANUAL&filterCriteria_depotLocationType=TRAINSTATION&filterCriteria_minimumFourDoors=true&filterCriteria_sortBy=PRICE&filterCriteria_sortAscending=true"
 
-    with driver:
+    with uc.Chrome() as driver:
         driver.get(link)
-        time.sleep(15)
+        print("Waiting for page to load")
+        WebDriverWait(driver, 20).until(
+            EC.text_to_be_present_in_element((By.CLASS_NAME, 'SM_3e7a1efe'), 'voitures disponibles'))
+        print("Page loaded or to")
         page_source = driver.page_source
 
     soup = BeautifulSoup(page_source, "html.parser")
@@ -26,6 +29,9 @@ def main():
     price_div = "SM_3e7a1efe SM_a81e959c"
 
     alll = soup.find_all("div", class_=car_div)
+    if not alll:
+        send_message("Probable bug in the program, found no cars. Check the website.")
+        return
 
     cheap_cars: list = []
     # find price in each div
