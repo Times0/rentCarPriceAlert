33d4dbe9856511e1799ff5e1f3fde25f6234fc8b
Times0 <45049767+Times0@users.noreply.github.com>
2024-03-21 03:18:33 +0100

    add buster

--- main.py
+++ main.py
@@ -27,7 +27,12 @@ def solve_captcha(driver):
     WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(
         (By.XPATH, "//iframe[starts-with(@src, 'https://www.google.com/recaptcha/api2/bframe')]")))
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".help-button-holder"))).click()
-    time.sleep(50)  # Wait for buster to solve captcha
+
+    driver.switch_to.default_content()
+    time.sleep(5)
+
+    if "captcha" in driver.page_source:
+        print("Captcha not solved :(")
 
 
 def main(driver):
